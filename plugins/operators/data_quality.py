da858f253ef52e542ab637398ee67a1625c01d9a
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-08-24 06:05:05 +0000

    Add operators

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -1,22 +1,34 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
 
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
 
-    @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id: str,
+                 table_name: str,
+                 id_column_name: str,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id,
+        self.table_name = table_name,
+        self.id_column_name = id_column_name
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info(f'Performing data quality check on "{self.table_name}"')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        
+        # Check if any rows were retrieved
+        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table_name}")
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Data quality check failed. {self.table_name} returned no results")
+        
+        # Check if id column is null
+        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table_name} WHERE {self.id_column_name} IS NULL")
+        num_of_records = records[0][0]
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f'Data quality check failed. Column "{self.id_column_name}" in table "{self.table_name}" contains {num_of_records:,} null values')        
+        
+        self.log.info(f'Data quality check on "{self.table_name}" passed')
--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -1,22 +1,45 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
-    @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id: str,
+                 table_name: str,
+                 sql: str,
+                 append_only: bool = False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id,
+        self.table_name = table_name,
+        self.sql = sql,
+        self.append_only = append_only
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info(f'Loading dimension table: "{self.table_name}"')
+        
+        if self.append_only:
+            self.log.info(f'Appending to table "{self.table_name}')
+            sql_insert_stmt = f'''
+                INSERT INTO {self.table_name}
+                {self.sql}
+            '''
+        else:
+            self.log.info(f'Deleting table "{self.table_name}"')
+            sql_del_stmt = f'DELETE FROM {self.table_name}'
+            redshift.run(sql_del_stmt)
+            self.log.info(f'Inserting into table "{self.table_name}"')
+            sql_insert_stmt = f'''
+                INSERT INTO {self.table_name}
+                {self.sql}
+            '''
+            redshift.run(sql_insert_stmt)
+
+
+            
+            
+            
